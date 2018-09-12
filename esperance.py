17dc105b541843fb40106c789b48a27ece2701e3
Flewer47 <36485170+Flewer47@users.noreply.github.com>
2018-09-12 13:44:09 +0200

    Calcul Espérance Ecart-type
    Pour différentes coordonnées de réaction

---
+++ esperance.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+import math
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+# variables
+numrep=10000
+numtrajectoires = 10 # Nombre de trajectoires qui seront tracees
+dt=0.01
+beta=6.67  # tester pour 300K ou alors beta = 6.67 ou 1.67
+nkill=1000
+number_killed = 0
+
+proba_theorique = 2e-7
+# initial points
+xi=-0.7
+yi=0.
+
+
+#%%####### FUNCTIONS ########
+# x=pos[0] | y=pos[1]
+# V(x,y)=0.2*x**4 + 0.2*(y-1.0/3)**4 + 3*exp(-x**2-(y-1.0/3)**2) - 3*exp(-x**2-(y-5.0/3)**2) - 5*exp(-(x-1)**2-y**2) - 5*exp(-(x+1)**2-y**2
+
+def V(x,y):
+    return 0.2*x**4 + 0.2*(y-1.0/3)**4 + 3*np.exp(-x**2-(y-1.0/3)**2) - 3*np.exp(-x**2-(y-5.0/3)**2) - 5*np.exp(-(x-1)**2-y**2) - 5*np.exp(-(x+1)**2-y**2) +4;
+
+# reaction coordinate: returns it for the last point
+def reccoord(ind, endroit):
+    d=0;
+    x=rep[ind][endroit][0];
+    y=rep[ind][endroit][1];
+    d = abs(x-1)
+    return 1/d ;# put here the reaction coordinate equation
+
+# returns the zone where the last point of the replica is
+def zone(ind):
+    # val=-1 if in A
+    # rep[ind][-1] est la dernière position de la particule d'indice ind
+    val=0
+    x=rep[ind][-1][0];
+    y=rep[ind][-1][1];
+
+    #si on est en A
+    if V(x,y)<0.5 and np.sqrt( (x+1)**2 + y**2 ) <= 0.25:
+        val = -1;
+    elif V(x,y)<0.5 and np.sqrt( (x-1)**2 + y**2 ) <= 0.25:
+        val = 1;
+    else :
+        val=0;
+    # val=1 if in B
+    # val=0 if otherwise
+    # on definit A et B comme des zones ou le niveau est < quelque chose et proche d'un certain point (pour ne pas confondre A et B)
+    return val
+
+# potential derivate
+def dPot(pos):
+    x=pos[0]
+    y=pos[1]
+    return [0.8*x**3-6*x*math.exp(-x**2-(y-1./3)**2) +
+                6*x*math.exp(-x**2-(y-5./3)**2) +
+                10*(x-1)*math.exp(-(x-1)**2-y**2) +
+                10*(x+1)*math.exp(-(x+1)**2-y**2) ,
+                0.8*(y-1./3)**3-6*(y-1./3)*math.exp(-x**2-(y-1./3)**2) +
+                6*(y-5./3)*math.exp(-x**2-(y-5./3)**2) +
+                10*y*math.exp(-(x-1)**2-y**2) +
+                10*y*math.exp(-(x+1)**2-y**2)] # put here the potential gradient
+
+# evolution function
+def run(ind):
+    global rep
+    # evolution for replica ind, return reaction coordinate
+    u=random.random()
+    v=random.random()
+    tgx=math.sqrt(2.*dt/beta)*math.sqrt(-2*math.log(u))*math.cos(2*math.acos(-1)*v)
+    tgy=math.sqrt(2.*dt/beta)*math.sqrt(-2*math.log(u))*math.sin(2*math.acos(-1)*v)
+    # ref: https://en.wikipedia.org/wiki/Normal_distribution#Generating_values_from_normal_distribution
+    tPot=dPot(rep[ind][-1])
+    rep[ind].append([rep[ind][-1][0]-dt*tPot[0]+tgx,rep[ind][-1][1]-dt*tPot[1]+tgy])
+    return reccoord(ind,-1)
+
+def replication(indice,alive_replicas, killing_level): 
+    #indice pointe dans rep au replica qui doit être supprimé
+    #killing_level est la valeur en-dessous de laquelle on supprime les réplicas
+    #ind est l'indice du replica à copier
+    #alive_replicas est une liste des replicas 
+    global rep
+    global level
+    rep[indice]=[]
+    ind=random.randint(0,len(alive_replicas)-1)
+    ind=alive_replicas[ind]
+    level[indice]=reccoord(ind,0) #initialisé pour être inférieur à killing_level
+    k=0
+    while level[indice]<=killing_level:
+        rep[indice].append(rep[ind][k])
+        k+=1
+        level[indice]=reccoord(ind,k)
+    rep[indice].append(rep[ind][k]) #point dont la reccoord est > à killing_level
+    #on poursuit la trajectoire de la particule correspondant au nouveau replica
+    while zone(indice) == 0:
+        where=run(indice)
+        if where > level[indice]:
+            level[indice]=where
+            
+    #%%#################################################################
+    # building the replicas structure
+
+compteur = 100
+X=[]
+PROBA=[]
+
+for o in range(compteur):
+    rep=[]
+    level=[]
+    for i in range(numrep):
+        rep.append([[xi,yi]])
+        level.append(reccoord(i,-1))
+    
+    # initialization of the replicas
+    for i in range(numrep):
+        while zone(i) == 0:
+            where=run(i)
+            if where > level[i]:
+                level[i]=where
+    
+    #AMS
+    zmax=4.255
+    nb_iterations=0.
+    proba=1.
+    while(True):
+        alive_replicas=[]
+        replicas_to_kill=[]
+        liste_triee=copy.deepcopy(level);
+        liste_triee.sort()
+        killing_level=liste_triee[nkill-1]
+    
+        if killing_level>=zmax:
+            break;
+        #on remplit les listes des replicas à tuer ou à conserver
+        for l in range(len(level)):
+            if level[l]<=killing_level:
+                replicas_to_kill.append(l)
+            else :
+                alive_replicas.append(l)
+        proba = proba * len(alive_replicas)/numrep
+    
+        number_killed += len(replicas_to_kill)
+    
+        if len(replicas_to_kill)==numrep :
+            break;
+        for indice in replicas_to_kill :
+            replication(indice,alive_replicas, killing_level)
+        nb_iterations+=1
+    
+    
+    
+    
+    #a la fin, on compte le nombre de replicas dans B
+    r=0
+    for k in range(numrep):
+        if zone(k)==1 :
+            r+=1;
+    proba = proba*r/numrep
+    
+    #print(nb_iterations)
+    #print(proba)
+    #print(number_killed)
+    
+    
+    arq = open('proba_d2.txt','a')
+    arq.write(str(proba) + '\n')
+    arq.close()
+    
+    X.append(o+1)
+    PROBA.append(proba)
+    print (o)
+
+
+
+
+plt.xlabel('Itérations de la simulation')
+plt.ylabel('Probabilité en moyenne')
+plt.title('Convergence en moyenne de la probabilité')
+plt.plot(X,[sum(PROBA[:i])/(i+1) for i in range(len(PROBA))])
+plt.show()
